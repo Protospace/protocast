6f112c00430e064ee5ae2cd0a86f2ca6ba91f7cc
Tanner Collin <git@tannercollin.com>
2025-06-17 19:19:10 -0600

    feat: add initial main script and logging

---
+++ main.py
@@ -0,0 +1,11 @@
+import os, logging
+DEBUG = os.environ.get('DEBUG')
+logging.basicConfig(
+        format='[%(asctime)s] %(levelname)s %(module)s/%(funcName)s - %(message)s',
+        level=logging.DEBUG if DEBUG else logging.INFO)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
